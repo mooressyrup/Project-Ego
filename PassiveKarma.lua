ac8c29e67fe5a16e73ce2cc548b682a208cb7918
Moore <mooressyrup@gmail.com>
2026-08-23 22:21:41 +0200

    Anti-Cheat Fix (10s instead of 2s)

--- PassiveKarma.lua
+++ PassiveKarma.lua
@@ -5,34 +5,34 @@ local enabled = false
 local runId = 0
 
 function PassiveKarma:SetEnabled(value)
-    if enabled == value then
-        return
-    end
+	if enabled == value then
+		return
+	end
 
-    enabled = value
-    runId += 1
+	enabled = value
+	runId += 1
 
-    if not enabled then
-        return
-    end
+	if not enabled then
+		return
+	end
 
-    local currentRunId = runId
-    task.spawn(function()
-        local scrollRemote = ReplicatedStorage:WaitForChild("Events2"):WaitForChild("Scroll")
+	local currentRunId = runId
+	task.spawn(function()
+		local scrollRemote = ReplicatedStorage:WaitForChild("Events2"):WaitForChild("Scroll")
 
-        while enabled and currentRunId == runId do
-            scrollRemote:FireServer("Start")
-            task.wait(2)
+		while enabled and currentRunId == runId do
+			scrollRemote:FireServer("Start")
+			task.wait(10)
 
-            if enabled and currentRunId == runId then
-                scrollRemote:FireServer("Give", "Order")
-            end
-        end
-    end)
+			if enabled and currentRunId == runId then
+				scrollRemote:FireServer("Give", "Order")
+			end
+		end
+	end)
 end
 
 function PassiveKarma:IsEnabled()
-    return enabled
+	return enabled
 end
 
 return PassiveKarma
